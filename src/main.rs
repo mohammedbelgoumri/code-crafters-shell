73efbd167657790cb0a7a670019fed72a1e4cb30
Mohammed Belgoumri <mohammedbelgoumri@gmail.com>
2026-06-20 22:56:19 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+enum Command {
+    Exit,
+    Echo(String),
+}
+
+fn parse(input: &str) -> Option<Command> {
+    if input == "exit" {
+        Some(Command::Exit)
+    } else {
+        input
+            .strip_prefix("echo")
+            .map(|message| Command::Echo(message.into()))
+    }
+}
+
 fn main() {
     loop {
         let mut cmd = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut cmd).unwrap();
-        cmd = cmd.trim().into();
-        if cmd == "exit" {
-            break;
-        } else {
-            println!("{}: command not found", cmd);
-            io::stdout().flush().unwrap();
+        match parse(cmd.trim()) {
+            Some(Command::Exit) => break,
+            Some(Command::Echo(message)) => {
+                println!("{message}");
+            }
+            None => {
+                println!("{}: command not found", cmd.trim());
+            }
         }
+        io::stdout().flush().unwrap();
     }
 }

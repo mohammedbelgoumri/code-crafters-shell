b7a0b79428d0c26d93e6a10d00e8ab445bd74cf8
Mohammed Belgoumri <mohammedbelgoumri@gmail.com>
2026-06-22 14:24:25 +1000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-
+use std::str::FromStr;
+#[derive(Debug)]
 enum Command {
     Exit,
     Echo(String),
+    Type(Box<Command>),
+}
+
+enum ParseCommandError {
+    Error(String),
+}
+
+impl FromStr for Command {
+    type Err = ParseCommandError;
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        if input == "exit" {
+            Ok(Command::Exit)
+        } else if let Some(messgae) = input.strip_prefix("echo ") {
+            Ok(Command::Echo(messgae.into()))
+        } else if let Some(cmd) = input.strip_prefix("type") {
+            let cmd: Command = cmd.parse()?;
+            Ok(Command::Type(Box::new(cmd)))
+        } else {
+            Err(ParseCommandError::Error(format!("{input}: not found")))
+        }
+    }
 }
 
 fn parse(input: &str) -> Option<Command> {
@@ -22,12 +44,15 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut cmd).unwrap();
-        match parse(cmd.trim()) {
-            Some(Command::Exit) => break,
-            Some(Command::Echo(message)) => {
+        match cmd.parse() {
+            Ok(Command::Exit) => break,
+            Ok(Command::Echo(message)) => {
                 println!("{message}");
             }
-            None => {
+            Ok(Command::Type(cmd)) => {
+                println!("{:?} is a shell builtin", cmd)
+            }
+            Err(ParseCommandError::Error(_)) => {
                 println!("{}: command not found", cmd.trim());
             }
         }
